a4956d19d3a71ac7591f55f6c9f6c1dffb180522
Takashi Takebayashi <changesworlds@gmail.com>
2023-03-30 12:57:23 +0900

    Add header component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { UserProvider } from '@auth0/nextjs-auth0/client'
+import Header from '../components/header'
 
 export default function App({ Component, pageProps }: AppProps) {
   const { user } = pageProps
   return (
     <UserProvider user={user}>
+      <Header />
       <Component {...pageProps} />
     </UserProvider>
   )
